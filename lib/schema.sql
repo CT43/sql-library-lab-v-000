c933caa0cb32f1b336e895d98493fd6786c84e7c
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-19 22:07:49 +0000

    automatically backed up by learn

---
+++ lib/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE characters (id PRIMARY KEY INTEGER, name TEXT, motto TEXT, species TEXT, author_id INTEGER, series_id INTEGER)
+CREATE TABLE books (id PRIMARY KEY INTEGER, title_year INTEGER, series_id INTEGER)
+CREATE TABLE series (id PRIMARY KEY INTEGER, title, TEXT, author_id INTEGER, subgenre_id INTEGER)
+CREATE TABLE authors (id PRIMARY KEY INTEGER, name TEXT)
+CREATE TABLE subgenres (id PRIMARY KEY INTEGER, name TEXT)
